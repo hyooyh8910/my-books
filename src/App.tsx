e36b912dcdfaeb0f38771e8206e7cc1172d754ea
hyooyh8910 <hyooyh8910@naver.com>
2022-08-24 19:19:28 +0900

    📂 docs. pages 파일추가, route설정
    add, detail, edit, home, notfound, signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import Detail from './pages/Detail'
+import Edit from './pages/Edit'
+import Home from './pages/Home'
+import Notfound from './pages/Notfound'
+import Signin from './pages/Signin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/book/:id" element={<Detail />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
